2be6a2e63b071f87da7ed2c90e338da95085f438
Simon Quigley <tsimonq2@lubuntu.me>
2018-01-27 22:51:00 -0600

    Add the initial setup file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import glob
+import os
+from DistUtilsExtra.command import *
+
+setup(name='lubuntu-about',
+      version='1.0',
+      packages=['src'],
+      scripts=['src/lubuntu-about'],
+      cmdclass = { "build" : build_extra.build_extra }
+)
